ba4d23c9e5d1f2d09ff703f8788eb8c5e2fa3588
Laura1608 <131393433+Laura1608@users.noreply.github.com>
2024-02-13 12:40:18 +0100

    Fine-tuning data extraction in loop

--- webscraping-project-fiverr.py
+++ webscraping-project-fiverr.py
@@ -1,9 +1,11 @@
+import numpy as np
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
 # Create empty list to store the scraped content
 data = []
+
 
 for i in range(1, 10):
     url = 'https://nl.fiverr.com/search/gigs?query=web%20scraping&source=pagination&acmpl=1&search_in=everywhere&search-autocomplete-original-term=web%20scraping&search-autocomplete-available=true&search-autocomplete-type=suggest&search-autocomplete-position=1&ref_ctx_id=ea69547a50e642d59ebb396d06c3ea08&page={i}&offset=-1'
@@ -23,28 +25,38 @@ for i in range(1, 10):
         title = services.find('h3', class_='QTdEgIS text-normal').text
 
         # Set standard to 'None' for rating, and replace with score if available
-        rating = 'None'
+        rating = np.NaN
         gig_rating = services.find('b', class_='rating-score iLiXwIR').text
         if gig_rating:
-            rating = gig_rating
+            rating = gig_rating.replace(',', '.')
+            rating = float(rating)
 
-        # Retrieve the amount of reviews
-        reviews_count = services.find('span', class_='rating-count-number').text
+        # Set standard to '0' for review counts, and replace with value if available
+        review_count = '0'
+        reviews = services.find('span', class_='rating-count-number').text
+        if reviews:
+            review_count = int(reviews)
 
-        # Set standard to 'None' for level, and replace with value if available
-        level = 'None'
+        # Set standard to '0' for level, and replace with value if available
+        level = '0'
         level_class = services.find('div', class_='tbody-6 text-semi-bold claXVc8')
         if level_class:
-            level = level_class.text
+            level = level_class.text.replace('Niveau ', '')
+            level = int(level)
 
         # Find the price element on the page
         price = services.find('span', class_='text-bold co-grey-1200').span
-        price = price.text.replace('\xa0', '')
+        price = price.text.replace('€\xa0', '')
+        price = int(price)
 
         # Add all variables to a list
-        data.append([title, rating, reviews_count, level, price])
+        data.append([title, rating, review_count, level, price])
 
 
 # Create a dataframe with columns
-column_names = ['title', 'rating', 'reviews_count', 'level', 'price']
+column_names = ['title', 'rating', 'review_count', 'level', 'price']
 df = pd.DataFrame(data, columns=column_names)
+print(df.head())
+
+df.to_csv('webscraping-fiverr-output.csv', index=False)
+print("Web Scraping Fiverr gigs extracted!")
