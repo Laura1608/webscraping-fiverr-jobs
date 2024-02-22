2517ddca5710d5200b1e7501b29279ad3857c99a
Laura1608 <131393433+Laura1608@users.noreply.github.com>
2024-02-22 10:47:51 +0100

    Adding pagination to scrape multiple pages

--- webscraping-project-fiverr.py
+++ webscraping-project-fiverr.py
@@ -5,110 +5,105 @@ from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
 
-# Set page number count from 1
-# i = 1
-#
-# while True:
-#
-# for i in range(1, 20):
-
-# Initialize WebDriver
-driver = webdriver.Firefox()
-
-# Create dictionary with user-agent = Firefox
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0', 'user_id': '', 'locale': 'en-US'}
-
-url = 'https://www.fiverr.com/search/gigs?query=scraping&source=pagination&ref_ctx_id=b7d6afc246514ab888c6631dfb5d6c7d&search_in=everywhere&search-autocomplete-original-term=scraping&page=2&offset=-1'
-driver.get(url)
-
-time.sleep(1)
-
-elem = driver.find_element('tag name', 'body')
-
-no_of_pagedowns = 4
-
-while no_of_pagedowns:
-    elem.send_keys(Keys.PAGE_DOWN)
-    time.sleep(0.2)
-    no_of_pagedowns -= 1
-
-# Fetch the data using BeautifulSoup after all data is loaded
-soup = BeautifulSoup(driver.page_source, "html.parser")
-
-# Close the WebDriver session
-driver.quit()
-
 # Create empty list to store the scraped content
 data_list = []
 
-# Loop over every Fiverr gig to retrieve its details
-for gigs in soup.find_all('div', class_='basic-gig-card'):
+# Create a loop to extract data from the first 5 webpages
+for i in range(1, 6):
 
-    # Find the title element on the page
-    title = gigs.find('h3', class_='QTdEgIS text-normal').text
+    # Initialize Selenium WebDriver
+    driver = webdriver.Firefox()
 
-    # Set standard to 'None' for rating, and replace with score if available
-    # Change datatype to float
-    rating = np.NaN
-    gig_rating = gigs.find('b', class_='rating-score iLiXwIR').text
-    if gig_rating:
-        rating = gig_rating.replace(',', '.')
-        rating = float(rating)
+    # Create dictionary with user-agent = Firefox as a header
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0', 'user_id': '', 'locale': 'en-US'}
 
-    # Set standard to '0' for review counts, and replace with value if available
-    review_count = '0 reviews'
-    reviews = gigs.find('span', class_='rating-count-number').text
-    if reviews:
-        review_count = reviews
+    # Connect to url through Selenium webdriver
+    url = 'https://www.fiverr.com/search/gigs?query=scraping&source=pagination&ref_ctx_id=b7d6afc246514ab888c6631dfb5d6c7d&search_in=everywhere&search-autocomplete-original-term=scraping&page={i}&offset=-1'
+    driver.get(url)
 
-    # Set standard to '0' for level, replace with value if available
-    # Change datatype to integer
-    level = 'Level 0'
-    level_class = gigs.find('div', class_='tbody-6 text-semi-bold claXVc8')
-    if level_class:
-        level = level_class.text
+    # Before performing next action, wait one second
+    time.sleep(2)
 
-    pro_badge = 'No Pro badge'
-    pro = gigs.find('span', class_='ucgAJ7j')
-    if pro:
-        pro_badge = "Pro badge"
+    # Select body element on webpage
+    elem = driver.find_element('tag name', 'body')
 
-    # Find out if video consultation is offered
-    video_cons = 'No video consultation'
-    consultation = gigs.find('p', class_='_1sz16f5k z58z871gm z58z871eo z58z877 z58z872')
-    if consultation:
-        video_cons = consultation.text
+    # Specify amount of scroll downs on page
+    no_of_pagedowns = 4
 
-    language = 'No extra languages'
-    seller_language = gigs.find('div', class_='seller-language')
-    if seller_language:
-        language = seller_language.span
+    # Create loop to scroll down on page automatically to retrieve its data
+    while no_of_pagedowns:
+        elem.send_keys(Keys.PAGE_DOWN)
+        time.sleep(0.2)
+        no_of_pagedowns -= 1
 
-    # Find the price element on the page
-    # Change datatype to integer
-    price = gigs.find('span', class_='text-bold co-grey-1200').span
-    price = price.text.replace('€\xa0', '').replace('€', '')
-    price = int(price)
+    # Fetch data using BeautifulSoup after all data is loaded
+    soup = BeautifulSoup(driver.page_source, "html.parser")
 
-    url = gigs.find('a', class_='tbody-5 p-t-8 lt2ar2q EhHcMiw')['href']
-    url = "https://nl.fiverr.com/" + url
+    # Close the WebDriver session
+    driver.quit()
 
-    # Add all variables to a list
-    data_list.append([title, rating, review_count, level, pro_badge, video_cons, language, price, url])
+    # Loop over gigs to retrieve its details
+    for gigs in soup.find_all('div', class_='basic-gig-card'):
 
+        # Find the title element on the page
+        title = gigs.find('h3', class_='QTdEgIS text-normal').text
 
-# # Add count to i, to find the next page
-# i = i + 1
-# if not i:
-#     break
+        # Set standard to 'None' for rating, and replace with score if available
+        # Change datatype to float
+        rating = np.NaN
+        gig_rating = gigs.find('b', class_='rating-score iLiXwIR').text
+        if gig_rating:
+            rating = gig_rating.replace(',', '.')
+            rating = float(rating)
+
+        # Set standard to '0' for review counts, and replace with value if available
+        review_count = '0 reviews'
+        reviews = gigs.find('span', class_='rating-count-number').text
+        if reviews:
+            review_count = reviews
+
+        # Set standard to '0' for level, and replace with value if available
+        # Change datatype to integer
+        level = 'Level 0'
+        level_class = gigs.find('div', class_='tbody-6 text-semi-bold claXVc8')
+        if level_class:
+            level = level_class.text
+
+        # Set standard to 'No' for the Pro badge, and replace with value if available
+        pro_badge = 'No Pro badge'
+        pro = gigs.find('span', class_='ucgAJ7j')
+        if pro:
+            pro_badge = "Pro badge"
+
+        # Set standard to 'No' for video consultation, and replace with value if available
+        video_cons = 'No video consultation'
+        consultation = gigs.find('p', class_='_1sz16f5k z58z871gm z58z871eo z58z877 z58z872')
+        if consultation:
+            video_cons = consultation.text
+
+        # Set standard to 'No' for extra languages spoken, and replace with value if available
+        language = 'No extra languages'
+        seller_language = gigs.find('div', class_='seller-language')
+        if seller_language:
+            language = seller_language.span
+
+        # Find the price element on the page
+        # Change datatype to integer
+        price = gigs.find('span', class_='text-bold co-grey-1200').span
+        price = price.text.replace('€\xa0', '').replace('€', '')
+        price = int(price)
+
+        # Find the url of every gig
+        url = gigs.find('a', class_='tbody-5 p-t-8 lt2ar2q EhHcMiw')['href']
+        url = "https://nl.fiverr.com/" + url
+
+        # Add all variables to a new list
+        data_list.append([title, rating, review_count, level, pro_badge, video_cons, language, price, url])
+
 
 # Create a dataframe with columns and print the first rows
 column_names = ['title', 'rating', 'review_count', 'level', 'pro_badge', 'video_cons', 'language', 'price', 'url']
 df = pd.DataFrame(data_list, columns=column_names)
 
+# Drop duplicated rows, based on url
 df = df.drop_duplicates(subset='url')
-
-print(df['pro_badge'].value_counts())
-print(df['language'].value_counts())
-
-print("\n Web Scraping Fiverr gigs extracted!")
